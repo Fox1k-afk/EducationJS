ec75d5f23bde5ad24e4fabd528080c5a2841511d
Vladyslav <Onoprienko.77@gmail.com>
2021-11-29 20:09:08 +0200

    lesson-3

---
+++ index.js
@@ -0,0 +1,7 @@
+let first_num =  prompt('Enter the first number');
+let second_num =  prompt('Enter the second number');
+
+// alert (Number (`${first_num}`) + Number(`${second_num}`));
+
+let result = (Number(`${first_num}`) + Number(`${second_num}`));
+alert(Number(result));
