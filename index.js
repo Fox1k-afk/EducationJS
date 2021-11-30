209881033ffcaaccc8f59ae9fd247b4ef7236790
Vladyslav <Onoprienko.77@gmail.com>
2021-11-30 14:34:58 +0200

    fixCalculating

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let first_num =  prompt('Enter the first number');
-let second_num =  prompt('Enter the second number');
+// let first_num =  prompt('Enter the first number');
+// let second_num =  prompt('Enter the second number');
 
-// alert (Number (`${first_num}`) + Number(`${second_num}`));
 
-let result = (Number(`${first_num}`) + Number(`${second_num}`));
-alert(Number(result));
+// let result = (Number(`${first_num}`) + Number(`${second_num}`));
+// alert(Number(result));
+
+
+let first_num =  +prompt('Enter the first number');
+let second_num =  +prompt('Enter the second number');
+
+let result = first_num + second_num;
+alert(`${first_num} + ${second_num} = ${result} `);
