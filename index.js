4455a014a7d21d335693242e887289df6b1f5f5c
Vladyslav <Onoprienko.77@gmail.com>
2021-12-08 13:50:39 +0200

    classWork

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-function cicles() {
-	let string = '';
-	let count = 0;
-while (true) {
-	const answer = prompt('Enter some text');
-	string = string + `${answer}`;
-	count++;
-	if (count % 3 == 0) {
-		let cont = confirm('Do we continue or not?')
-		if (cont == true) {
-			continue;
-		} else {
-			alert('Game Over');
-			break;
-		}
-	}
-	console.log(string);
-	}
+// function cicles() {
+// 	let string = '';
+// 	let count = 0;
+// while (true) {
+// 	const answer = prompt('Enter some text');
+// 	string = string + `${answer}`;
+// 	count++;
+// 	if (count % 3 == 0) {
+// 		let cont = confirm('Do we continue or not?')
+// 		if (cont == true) {
+// 			continue;
+// 		} else {
+// 			alert('Game Over');
+// 			break;
+// 		}
+// 	}
+// 	console.log(string);
+// 	}
+// }
+// cicles();
+
+function array() {
+ let strings = [];
+ let numbers = [];
+ let count = 0;
+ let isConfirm = true;
+
+ while (true) {
+  let question = prompt("Choose: word or number");
+  const isNumber = +question;
+  if (isNaN(isNumber)) {
+   strings.push(question);
+  } else {
+   numbers.push(question);
+  }
+  if (count % 5 === 0 && count != 0) {
+   isConfirm = confirm("Continue or not?");
+  }
+
+  if (!isConfirm) {
+   break;
+  }
+  count++;
+ }
+ console.log(strings);
+ console.log(numbers);
 }
-
-cicles();
-
+array();
