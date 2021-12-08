9cfc10c76cecfbca3f9b01ba931e4cd776c64118
Vladyslav <Onoprienko.77@gmail.com>
2021-12-08 14:05:30 +0200

    fixedHomeWork

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
+// function cicles() {
+// 	let string = '';
+// 	let count = 0;
+// while (true) {
+// 	const answer = prompt('Enter some text');
+// 	string = string + `${answer}`;
+// 	count++;
+// 	if (count % 3 == 0) {
+// 		let cont = confirm('Do we continue or not?')
+// 		if (cont == true) {
+// 			continue;
+// 		} else {
+// 			alert('Game Over');
+// 			break;
+// 		}
+// 	}
+// 	console.log(string);
+// 	}
+// }
+
+// cicles();
+
 function cicles() {
-	let string = '';
-	let count = 0;
-while (true) {
-	const answer = prompt('Enter some text');
-	string = string + `${answer}`;
-	count++;
-	if (count % 3 == 0) {
-		let cont = confirm('Do we continue or not?')
-		if (cont == true) {
-			continue;
-		} else {
-			alert('Game Over');
-			break;
-		}
-	}
-	console.log(string);
-	}
+ let string = "";
+ let count = 0;
+ let isConfirm = true;
+ while (true) {
+  const answer = prompt("Enter some text");
+  string = string + `${answer}`;
+  if (count % 3 === 0 && count != 0) {
+   isConfirm = confirm("Do we continue or not?");
+  }
+  if (!isConfirm) {
+   break;
+  }
+  count++;
+  console.log(string);
+ }
 }
 
 cicles();
-
