bb808179d1eb94acd00cabc9eb687370d3b7d86f
Vladyslav <Onoprienko.77@gmail.com>
2021-12-08 14:03:22 +0200

    fixedClassWork

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-// function cicles() {
-// 	let string = '';
-// 	let count = 0;
-// while (true) {
-// 	const answer = prompt('Enter some text');
-// 	string = string + `${answer}`;
-// 	count++;
-// 	if (count % 3 == 0) {
-// 		let cont = confirm('Do we continue or not?')
-// 		if (cont == true) {
-// 			continue;
-// 		} else {
-// 			alert('Game Over');
-// 			break;
-// 		}
-// 	}
-// 	console.log(string);
-// 	}
-// }
-// cicles();
-
-function array() {
- let strings = [];
- let numbers = [];
+function cicles() {
+ let string = "";
  let count = 0;
  let isConfirm = true;
-
  while (true) {
-  let question = prompt("Choose: word or number");
-  const isNumber = +question;
-  if (isNaN(isNumber)) {
-   strings.push(question);
-  } else {
-   numbers.push(question);
+  const answer = prompt("Enter some text");
+  string = string + `${answer}`;
+  if (count % 3 === 0 && count != 0) {
+   isConfirm = confirm("Do we continue or not?");
   }
-  if (count % 5 === 0 && count != 0) {
-   isConfirm = confirm("Continue or not?");
-  }
-
   if (!isConfirm) {
    break;
   }
   count++;
+  console.log(string);
  }
- console.log(strings);
- console.log(numbers);
 }
-array();
+
+cicles();
+
+// function array() {
+//  let strings = [];
+//  let numbers = [];
+//  let count = 0;
+//  let isConfirm = true;
+
+//  while (true) {
+//   let question = prompt("Choose: word or number");
+//   const isNumber = +question;
+//   if (isNaN(isNumber)) {
+//    strings.push(question);
+//   } else {
+//    numbers.push(question);
+//   }
+//   if (count % 5 === 0 && count != 0) {
+//    isConfirm = confirm("Continue or not?");
+//   }
+
+//   if (!isConfirm) {
+//    break;
+//   }
+//   count++;
+//  }
+//  console.log(strings);
+//  console.log(numbers);
+// }
+// array();
