628a7517d696341cb102461c72e6679a9d84d166
Vladyslav <Onoprienko.77@gmail.com>
2021-12-10 16:18:57 +0200

    task2Done

--- index.js
+++ index.js
@@ -27,28 +27,34 @@
 
 // START 1 TASK
 
-// function arrAdd() {
-//  let bank = [];
-//  while (true) {
-//   let question = prompt("Enter some text");
-//   bank.push(question);
-//   console.log(bank);
-//   if (!question || question === "") {
-//    console.log(bank.pop());
-//   }
-//  }
-// }
-// arrAdd();
+function arrAdd() {
+ let bank = [];
+ while (true) {
+  let question = prompt("Enter some text");
+  bank.push(question);
+  console.log(bank);
+  if (!question || question === "") {
+   console.log(bank.pop());
+  }
+ }
+}
+arrAdd();
 
 // END 1 TASK
 
 // START 2 TASK
 
-function hundred() {
- let vault = [];
- for (let i = 0; i < 65; i++) {
-  vault.push(i);
-  console.log(vault);
- }
-}
-hundred();
+// function hundred() {
+//  const vault = [];
+//  for (let i = 0; i < 100; i++) {
+//   vault.push(i);
+//  }
+//  console.log(vault);
+//  const newVault = vault.map(function (item, index, array) {
+//   return item * 3;
+//  });
+//  console.log(newVault);
+// }
+// hundred();
+
+// END 2 TASK
