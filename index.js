45b18ea7576debeb2382d26899429a538854c4e0
Vladyslav <Onoprienko.77@gmail.com>
2021-12-10 19:41:00 +0200

    fixSecondTask2Work

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // START 1 TASK
 
-function arrAdd() {
- let bank = [];
- while (true) {
-  const question = prompt("Enter some text");
-  if (question === null || question === "") {
-   bank.pop();
-  } else {
-   bank.push(question);
-  }
-  console.log(bank);
- }
-}
-arrAdd();
+// function arrAdd() {
+//  let bank = [];
+//  while (true) {
+//   const question = prompt("Enter some text");
+//   if (question === null || question === "") {
+//    bank.pop();
+//   } else {
+//    bank.push(question);
+//   }
+//   console.log(bank);
+//  }
+// }
+// arrAdd();
 
 // END 1 TASK
 
 // START 2 TASK
 
-// function hundred() {
-//  const vault = [];
-//  for (let i = 0; i < 100; i++) {
-//   vault.push(i);
-//  }
-//  console.log(vault);
-//  const newVault = vault.map(function (item, index, array) {
-//   return item * 3;
-//  });
-//  console.log(newVault);
-// }
-// hundred();
+function hundred() {
+ const vault = [];
+ for (let i = 1; i <= 100; i++) {
+  vault.push(i);
+ }
+ console.log(vault);
+ const newVault = vault.map(function (item, index, array) {
+  return item * 3;
+ });
+ console.log(newVault);
+}
+hundred();
 
 // END 2 TASK
 
