627c84060f112f2579dd647038d0cdeff237d8df
Vladyslav <Onoprienko.77@gmail.com>
2021-12-21 15:56:28 +0200

    capitilizeDone

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // START 1 TASK
 
-function arrAdd() {
- let bank = [];
- while (true) {
-  let question = prompt("Enter some text");
-  bank.push(question);
-  console.log(bank);
-  if (!question || question === "") {
-   console.log(bank.pop());
-  }
- }
-}
-arrAdd();
+// function arrAdd() {
+//  let bank = [];
+//  while (true) {
+//   const question = prompt("Enter some text");
+//   if (question === null || question === "") {
+//    bank.pop();
+//   } else {
+//    bank.push(question);
+//   }
+//   console.log(bank);
+//  }
+// }
+// arrAdd();
 
 // END 1 TASK
 
@@ -19,7 +20,7 @@ arrAdd();
 
 // function hundred() {
 //  const vault = [];
-//  for (let i = 0; i < 100; i++) {
+//  for (let i = 1; i <= 100; i++) {
 //   vault.push(i);
 //  }
 //  console.log(vault);
@@ -62,3 +63,36 @@ arrAdd();
 // array();
 
 ////////////////////////////////////////////////////////////////////////////////////
+
+// let string =
+//  "Lorem ipsum fuck suck dolor sit amet, consectetur adipisicing elit. Expedita praesentium nemo fuck suck totam quibusdam natus nesciunt tempora nam. Harum rem molestias repudiandae accusamus ab, esse nihil officiis explicabo iste.";
+
+// function filtration(str) {
+//  let arr = str.split(" ");
+//  let filtered = arr.filter(words);
+//  function words(result) {
+//   if (result !== "fuck" && result !== "suck") {
+//    return result;
+//   }
+//  }
+//  return filtered.join(" ");
+// }
+// filtration(string);
+
+// const result = filtration(string);
+// console.log(result);
+
+// /////////////////////////////////////////////////////////////////////////////////
+
+function capitalize(str) {
+ let arr = str.split(" ");
+ let newArr = arr.map(function (item) {
+  return item.charAt(0).toUpperCase() + item.slice(1);
+ });
+ const capitalString = newArr.join(" ");
+ console.log(capitalString);
+}
+const string =
+ "Lorem ipsum fuck suck dolor sit amet, ae ae ae ae ae ae ae a ea ea ea consectetur adipisicing elit. Expedita praesentium nemo fuck suck totam quibusdam natus nesciunt tempora nam. Harum rem molestias repudiandae accusamus ab, esse nihil officiis explicabo iste.";
+
+capitalize(string);
